e5dfde0a69dcfdd3dc8a977b4fb397cbce25d98c
emircan-yilmaz <emircany248@gmail.com>
2022-01-05 16:30:10 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import Row from "./Row";
+import requests from "./request";
+import Banner from "./Banner";
+import Nav from "./Nav";
+
+function App() {
+  return (
+    <div className="app">
+      <Nav />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+    </div>
+  );
+}
+
+export default App;
